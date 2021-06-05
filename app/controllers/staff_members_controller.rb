54e101494a6e6a2a188e4c29a656793ccd36153c
ka-jun <teraguchi51@gmail.com>
2021-06-05 12:00:49 +0900

    マイページ１

--- app/controllers/staff_members_controller.rb
+++ app/controllers/staff_members_controller.rb
@@ -2,20 +2,11 @@ class StaffMembersController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
-    @staff_members = StaffMember.order('created_at DESC')
-  end
-
-  def new
-    @staff_member = StaffMember.new(staff_member_params)
-  end
-
-  def create
-    @staff_member = StaffMember.new(staff_member_params)
-    redirect_to root_path
+    @staff_members = StaffMember.all
   end
 
   def show
-
+    # @staff_member = StaffMember.find(params[:id])
   end
 
   def edit
@@ -24,8 +15,7 @@ class StaffMembersController < ApplicationController
   private
 
   def staff_member_params
-    binding.pry
     params.require(:staff_member).permit(:image, :last_name, :first_name, :area, :since, :text)
-    .merge(staff_member_id: current_user.id)
+    .merge(staff_member_id: current_staff_member.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :staff_members
   root to: "staff_members#index"
+
+  resources :staff_members
   
 
 end
