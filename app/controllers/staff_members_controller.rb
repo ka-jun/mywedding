a25c41373faef2d1ae931b6f5502aeae5fe8c57b
ka-jun <teraguchi51@gmail.com>
2021-06-08 21:26:00 +0900

    HTML修正

--- app/controllers/staff_members_controller.rb
+++ app/controllers/staff_members_controller.rb
@@ -1,11 +1,13 @@
 class StaffMembersController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_staff_menber, only: :show
 
   def index
     @staff_members = StaffMember.order('created_at DESC')
   end
 
   def show
+    
   end
 
   def edit
@@ -16,4 +18,9 @@ class StaffMembersController < ApplicationController
   def staff_member_params
     params.require(:staff_member).permit(:image, :last_name, :first_name, :area, :since, :text)
   end
+
+  def set_staff_menber
+    @staff_member = StaffMember.find(params[:id])
+  end
+
 end
