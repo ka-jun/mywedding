02372edda2db381828155ebdd5759f13c249726f
ka-jun <teraguchi51@gmail.com>
2021-06-19 10:57:14 +0900

    マイページの制限

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2021_06_12_013441) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "message", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "staff_members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
