5b58c3538b07af253441241d69f37c2ec629c849
ka-jun <teraguchi51@gmail.com>
2021-06-27 07:49:27 +0900

    テストコード

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -9,56 +9,48 @@ RSpec.describe StaffMember, type: :model do
       it 'last_nameが空では登録できない' do
         @staff_member.last_name = ''  
         @staff_member.valid?
-        binding.pry
         expect(@staff_member.errors.full_messages).to include  "Last name can't be blank", "Last name is invalid"
       end
       it 'first_nameが空では登録できない' do
         @staff_member.first_name = ''  
         @staff_member.valid?
-        binding.pry
         expect(@staff_member.errors.full_messages).to include "First name can't be blank", "First name is invalid"
       end
       it 'emailが空では登録できない' do
         @staff_member.email = ''  
         @staff_member.valid?
-        binding.pry
         expect(@staff_member.errors.full_messages).to include "Email can't be blank"
       end
       it 'passwordが空では登録できない' do
-        @staff_member.passworde = ''
+        @staff_member.password = ''
         @staff_member.valid?
         binding.pry
-        # expect(@staff_member.errors.full_messages).to include "First name1 can't be blank"
+        expect(@staff_member.errors.full_messages).to include "Password can't be blank", "Password can't be blank", "Password is invalid", "Password confirmation doesn't match Password"
       end
       it 'password_confirmationが空では登録できない' do
         @staff_member.password_confirmation = ''  
         @staff_member.valid?
-        binding.pry
-        # expect(@staff_member.errors.full_messages).to include "First name1 can't be blank"
+        expect(@staff_member.errors.full_messages).to include "Password confirmation doesn't match Password"
       end
       it 'areaが空では登録できない' do
         @staff_member.area = ''  
         @staff_member.valid?
-        binding.pry
-        # expect(@staff_member.errors.full_messages).to include "First name1 can't be blank"
+        expect(@staff_member.errors.full_messages).to include "Area can't be blank"
       end
       it 'sinceが空では登録できない' do
         @staff_member.since = ''  
         @staff_member.valid?
-        binding.pry
-        # expect(@staff_member.errors.full_messages).to include "First name1 can't be blank"
+        expect(@staff_member.errors.full_messages).to include "Since can't be blank"
       end
       it 'textが空では登録できない' do
         @staff_member.text = ''  
         @staff_member.valid?
-        binding.pry
-        # expect(@staff_member.errors.full_messages).to include "First name1 can't be blank"
+        expect(@staff_member.errors.full_messages).to include "Text can't be blank"
       end
       it 'imageが空では登録できない' do
         @staff_member.image = nil  
         @staff_member.valid?
-        binding.pry
-        # expect(@staff_member.errors.full_messages).to include "Last name can't be blank"
+        expect(@staff_member.errors.full_messages).to include "Image can't be blank"
       end
     end
   end
