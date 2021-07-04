d21637893e116998e3fdd36c90fa35cbd6e829b6
ka-jun <teraguchi51@gmail.com>
2021-07-04 22:07:25 +0900

    Herokuエラー修正２

--- app/controllers/staff_members_controller.rb
+++ app/controllers/staff_members_controller.rb
@@ -13,7 +13,7 @@ class StaffMembersController < ApplicationController
   end
 
   def show
-    # @contact = Contact.new
+    @contact = Contact.new
     # @contact = Contact.find(params[:id])
     @contacts = @staff_member.contacts.includes(:user)
   end
